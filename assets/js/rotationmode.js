86a255f5b18fc85f9a3e8f6449387aa98249ad9f
Hsterts <62886439+Hsterts@users.noreply.github.com>
2022-11-12 17:15:08 +0800

    catching errors

--- assets/js/rotationmode.js
+++ assets/js/rotationmode.js
@@ -3,13 +3,19 @@ function hideElement(element){element.style.display = 'inline-block'}
 
 function switchRotation(){
     var toggle = document.getElementById('rotationmode-toggle').checked
+    var Icon180 = document.getElementById('90icon')
+    var Icon90 = document.getElementById('180icon')
     var Rot180 = document.getElementsByClassName('Rot180')
     var Rot90 = document.getElementsByClassName('Rot90')
     if(toggle){
-        for(element in Rot180){hideElement(element)}
-        for(element in Rot90){showElement(element)}
+        if(Rot180.length != 0){for(element in Rot180){hideElement(element)}}
+        if(Rot90.length != 0){for(element in Rot90){showElement(element)}}
+        Icon180.style.opacity = 1
+        Icon90.style.opacity = 0
     } else {
-        for(element in Rot180){showElement(element)}
-        for(element in Rot90){hideElement(element)}
+        if(Rot180.length != 0){for(element in Rot180){showElement(element)}}
+        if(Rot90.length != 0){for(element in Rot90){hideElement(element)}}
+        Icon180.style.opacity = 0
+        Icon90.style.opacity = 1
     }
 }
