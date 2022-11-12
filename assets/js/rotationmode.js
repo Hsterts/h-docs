19fd9500f2b227e40cf4106f79d128d918e99342
Hsterts <62886439+Hsterts@users.noreply.github.com>
2022-11-12 23:51:01 +0800

    Fixed hover tooltip for 180 button

--- assets/js/rotationmode.js
+++ assets/js/rotationmode.js
@@ -1,11 +1,13 @@
 function switchRotation(){
     var toggle = document.getElementById('rotationmode-toggle').checked
+    var rotationModeDiv = document.getElementsByClassName('rotationmode')[0]
     var Icon180 = document.getElementById('90icon')
     var Icon90 = document.getElementById('180icon')
     var Rot180 = document.getElementsByClassName('Rot180')
     var Rot90 = document.getElementsByClassName('Rot90')
     if(toggle){
         console.log('Disabled 180s')
+        rotationModeDiv.setAttribute('title','Turn on 180 rotations. Primarily affects percentages.')
         if(Rot180.length != 0){
             for(let i = 0; i < Rot180.length; i++){
                 Rot180[i].style.display = 'none'
@@ -20,6 +22,7 @@ function switchRotation(){
         Icon90.style.opacity = 0
     } else {
         console.log('Enabled 180s')
+        rotationModeDiv.setAttribute('title','Turn off 180 rotations. Primarily affects percentages.')
         if(Rot180.length != 0){
             for(let i = 0; i < Rot180.length; i++){
                 Rot180[i].style.display = 'inline-block'
--- content/perfect-clears/1st/First PC.md
+++ content/perfect-clears/1st/First PC.md
@@ -5,7 +5,7 @@ title: First Perfect Clear
 ___
 ## Setups
 <figure class = "Rot180">
-	<fumen>9gzhFeh0BtFeg0RpBtEeg0RpQeAgH</fumen>
+	<fumen>v115@9gzhFeh0BtFeg0RpBtEeg0RpQeAgH</fumen>
 	<figcaption><span title = "4956">Sol%: 98.333%</span></figcaption>
 </figure>
 <figure class = "Rot90">
